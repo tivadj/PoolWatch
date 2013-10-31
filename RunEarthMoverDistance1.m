1c79a6db82140bedde41d93ae33b9e2cafa31ce0
tivadj <tivadj@gmail.com>
2013-10-31 23:02:26 +0200

    optimized by removing loops; added histogram trimming

--- RunEarthMoverDistance1.m
+++ RunEarthMoverDistance1.m
@@ -8,9 +8,10 @@ end
 
 function run(obj, debug)
     %RunEarthMoverDistance1.simple1(obj, debug);
-    %RunEarthMoverDistance1.testEmdOnCoupleOfFiles(obj, debug);
+    %RunEarthMoverDistance1.testRgbConversion(obj, debug);    
     
     RunEarthMoverDistance1.testEmd1(obj, debug);
+    %RunEarthMoverDistance1.analyzeHistogramDistributions(obj,debug);
 end
 
 function simple1(obj, debug)
@@ -23,20 +24,25 @@ function simple1(obj, debug)
 end
 
 function testRgbConversion(obj, debug)
+    pixs=[170 187 204; 171 188 205];
+    
+    pixs32 = RunEarthMoverDistance1.rgb2uint32(pixs);
+    
     r=0;
     g=0;
     b=0;
-    %
-    rgb32 = rgb2uint32(170,187,204)
-    [r g b]=uint322rgb(cast(2864434176,'uint32'))
+    [r g b]=RunEarthMoverDistance1.uint322rgb(pixs32)
 end
 
 function testEmd1(obj, debug)
     appearDirPath = '../dinosaur/appear1';
     bgPixel = [0 0 0];
+    cellSizePixels = 3; % power of 2
+    histInclMin = 1; % pixels with lesser occurence number are not incuded in histogram
+    fprintf('qunatization cellSizePixels=%d\n', cellSizePixels);
 
     if ~isfield(obj.v, 'appearModel')
-        obj.v.appearModel = RunEarthMoverDistance1.learnAppearModelsFromDirectoryStructure(obj, appearDirPath, bgPixel, debug);
+        obj.v.appearModel = RunEarthMoverDistance1.learnAppearModelsFromDirectoryStructure(obj, appearDirPath, bgPixel, cellSizePixels, histInclMin, debug);
     end
 
     if ~isfield(obj.v, 'testHistMats')
@@ -53,23 +59,32 @@ function testEmd1(obj, debug)
             groupRgbHist = containers.Map('KeyType','uint32','ValueType','uint32');
 
             filePath = testFiles{i}{1};
-            RunEarthMoverDistance1.accumulateRgbHistogramFromImageFile(groupRgbHist, filePath, bgPixel);
+            RunEarthMoverDistance1.accumulateRgbHistogramFromImageFile(groupRgbHist, filePath, bgPixel, cellSizePixels);
 
+            %RunEarthMoverDistance1.trimHistogram(groupRgbHist, histInclMin);
             groupRgbHistMat = RunEarthMoverDistance1.rgbHistogramToArray(groupRgbHist);
             testHistMats{end+1} = groupRgbHistMat;
         end
         obj.v.testHistMats = testHistMats;
     end
     
-    RunEarthMoverDistance1.testAppearModelOnCandidates(obj.v.appearModel, obj.v.testHistMats);
+    RunEarthMoverDistance1.testAppearModelOnOtherShapes(obj.v.appearModel, obj.v.testHistMats);
+end
+
+function testEmd1Clear(obj)
+    if isfield(obj.v, 'appearModel')
+        obj.v = rmfield(obj.v, 'appearModel');
+    end
+    
+    if isfield(obj.v, 'testHistMats')
+        obj.v = rmfield(obj.v, 'testHistMats');
+    end
 end
 
 % groupsDir = directory which contains folder per each target
 % Each such folder contains images of shapes of single target.
-function appearModel = learnAppearModelsFromDirectoryStructure(obj, appearDir, bgPixel, debug)
+function appearModel = learnAppearModelsFromDirectoryStructure(obj, appearDir, bgPixel, cellSizePixels, histInclMin, debug)
     appearDirItems = dir(appearDir);
-%     names = {appearDir.name};
-%     names = names([appearDir.isdir]);
     
     % find name of targets; they are encoded as directory name
     targetsName = {};
@@ -86,6 +101,7 @@ function appearModel = learnAppearModelsFromDirectoryStructure(obj, appearDir, b
     appearModel = containers.Map('KeyType','char','ValueType','any');
     for groupNameInd = 1:length(targetsName)
         groupName = targetsName{groupNameInd};
+        fprintf('processing group=%s\n', groupName);
         
         groupRgbHist = containers.Map('KeyType','uint32','ValueType','uint32');
         
@@ -93,9 +109,14 @@ function appearModel = learnAppearModelsFromDirectoryStructure(obj, appearDir, b
         for fileInd=1:length(files)
             file = files(fileInd);
             filePath = fullfile(appearDir, groupName, file.name);
+            fprintf('shape pixels=%s ', filePath);
             
-            RunEarthMoverDistance1.accumulateRgbHistogramFromImageFile(groupRgbHist, filePath, bgPixel);
+            tic;
+            RunEarthMoverDistance1.accumulateRgbHistogramFromImageFile(groupRgbHist, filePath, bgPixel, cellSizePixels);
+            fprintf('took %.2f sec\n', toc);
         end
+        
+        RunEarthMoverDistance1.trimHistogram(groupRgbHist, histInclMin);
         
         groupRgbHistMat = RunEarthMoverDistance1.rgbHistogramToArray(groupRgbHist);
         
@@ -103,126 +124,84 @@ function appearModel = learnAppearModelsFromDirectoryStructure(obj, appearDir, b
     end
 end
 
-function testEmdOnCoupleOfFiles(obj, debug)
-%     fileGroups={{'MVI_3177_175_woman_black','MVI_3177_2047_woman_black'},
-%     {'MVI_3178_602_black1','MVI_3178_1913_black1'},
-%     {'MVI_3178_2297_kid1','MVI_3178_2453_kid1'}};
+% removes from histogram map all values less than histInclMin.
+% Histogram is passed by reference.
+function trimHistogram(groupRgbHist, histInclMin)
+    groupRgbHistValues = groupRgbHist.values;
+    exclPixelMask = cell2mat(groupRgbHistValues) < histInclMin;
 
-    if (~isfield(obj.v, 'appearModel'))
-        RunEarthMoverDistance1.learnAppearModelsOnCoupleOfFiles(obj, fileGroups, debug);
-    end
-
-    fprintf('finding distances between histograms\n');
-
-    %!!!
-    groupsCount = length(obj.v.appearModel);
-    distMat = zeros(groupsCount,groupsCount);
-    % process matrix upper triangle
-    for row=1:(groupsCount-1)
-    for col=(row+1):groupsCount
-        imageBarRecordA = obj.v.appearModel(int2str(row));
-        imageBarRecordB = obj.v.appearModel(int2str(col));
-
-        tic;
-        dist=cv.EMD(imageBarRecordA, imageBarRecordB);
-        fprintf('dist(%d,%d)=%.4f found in %.2f sec. S1=%d S2=%d\n', row, col, dist, toc, length(imageBarRecordA), length(imageBarRecordB));
-
-        distMat(row,col) = dist;
-    end
-    end
-    display(distMat);
-
-    % MVI_3177_175_woman_black.svg
-    % MVI_3177_2047_woman_black.svg
-    % MVI_3178_1913_black1.svg
-    % MVI_3178_2297_kid1.svg
-    % MVI_3178_2453_kid1.svg
-    % MVI_3178_602_black1.svg
-    %          0    4.6321   43.0203   25.5680   36.3205   50.2176
-    %          0         0   12.7576    6.4162    7.8527   10.6642
-    %          0         0         0   10.8882   19.5881   23.1406
-    %          0         0         0         0   13.3240   11.7520
-    %          0         0         0         0         0   16.8781
-    %          0         0         0         0         0         0
-end
-
-
-function learnAppearModelsOnCoupleOfFiles(obj, fileGroups, debug)
-    appearDir = fullfile('data/appearance');
-    %appearFilesPattern = fullfile(appearDir, '*.svg');
-    %svgFiles = dir(appearFilesPattern);
-
-    % construct histograms for each image
-    groupsCount = length(fileGroups);
-
-    % map between shape name and its histogram (Nx4), N=number of pixels
-    % 4elements are = [occur R G B]
-    appearModel = containers.Map('KeyType','char','ValueType','any');
-
-    for groupInd=1:groupsCount
-        fprintf('processing image group=%d\n', groupInd);
-        groupName = int2str(groupInd);
-
-        groupRgbHist = containers.Map('KeyType','uint32','ValueType','uint32');
-
-        group = fileGroups{groupInd};
-        for fileNameInd=1:length(group)
-            fileName = group{fileNameInd};
-            filePath = fullfile(appearDir, strcat(fileName, '.svg'));
-            pixels = utils.getPixelsFromLabelledImage(filePath, false, '#00FFFF');
-
-            RunEarthMoverDistance1.accumulatePixelsInRgbHistogramMap(groupRgbHist, pixels);
-        end
-
-        fprintf(1, 'rgbHist constructed in % sec\n', toc);
-
-        histBarRecordMat = RunEarthMoverDistance1.rgbHistogramToArray(groupRgbHist);
-
-        appearModel(groupName) = histBarRecordMat;
-    end
-    
-    obj.v.appearModel = appearModel;
+    groupRgbHistKeys = cell2mat(groupRgbHist.keys);
+    exclPixelKeys = groupRgbHistKeys(exclPixelMask);
+    groupRgbHist.remove(num2cell(exclPixelKeys));
 end
 
 % convert hashtable to histogram bar record
 function histBarRecordMat = rgbHistogramToArray(groupRgbHist)
-    histBarRecordMat = zeros(length(groupRgbHist),4,'single'); % Occur R G B
+    rgb32 = cell2mat(groupRgbHist.keys)';
+    
+    [r1 g1 b1] = RunEarthMoverDistance1.uint322rgb(rgb32);
 
-    rgbHistKeys = keys(groupRgbHist);
-    i=1;
-    for rgb32=[rgbHistKeys{:}]
-        occur = groupRgbHist(rgb32);
-        [r1 g1 b1] = RunEarthMoverDistance1.uint322rgb(rgb32);
-
-        histBarRecordMat(i,:) = single([occur r1 g1 b1]);
-        i=i+1;
-    end
+    occur = cell2mat(groupRgbHist.values)';
+    
+    % Occur R G B
+    histBarRecordMat = single([occur r1 g1 b1]);
 end
 
-function accumulateRgbHistogramFromImageFile(groupRgbHist, imageFilePath, transpPixel)
+function accumulateRgbHistogramFromImageFile(groupRgbHist, imageFilePath, transpPixel, cellSizePixels)
     img = imread(imageFilePath);
     pixels = reshape(img, [], 3);
 
     % remove black pixels
-    utils.PW.matRemoveIf(pixels, @(rgb) rgb(1) == transpPixel(1) && rgb(2) == transpPixel(2) && rgb(3) == transpPixel(3));
+    %pixels = utils.PW.matRemoveIf(pixels, @(rgb) all(rgb == transpPixel));
+    
+    transpPixelMat = repmat(transpPixel, size(pixels,1), 1);
+    rowIndsToRem = sum(pixels == transpPixelMat, 2) == 3;
+    pixels(rowIndsToRem, :) = [];
 
-    RunEarthMoverDistance1.accumulateRgbHistogramFromPixels(groupRgbHist, pixels);
+    RunEarthMoverDistance1.accumulateRgbHistogramFromPixels(groupRgbHist, pixels, cellSizePixels);
 end
 
-function accumulateRgbHistogramFromPixels(groupRgbHist, pixels)
+function accumulateRgbHistogramFromPixels(groupRgbHist, pixels, cellSizePixels)
+    % quntize - put pixels in cells of specified size
+    % the information is lost but processing perf increases
+    cells = fix(pixels ./ cellSizePixels) + 1;
+    
+    cells32 = RunEarthMoverDistance1.rgb2uint32(cells);
+    
+    one32 = cast(1, 'uint32');
+    arrayfun(@(cell32) RunEarthMoverDistance1.containersMapIncValue(groupRgbHist, cell32, one32), cells32);
+end
+
+% Helper for map(key) += incValue. Map is processed by reference.
+function containersMapIncValue(map, key, incValue)
+    if map.isKey(key)
+        map(key) = map(key) + incValue;
+    else
+        map(key) = incValue;
+    end
+end
+
+function accumulateRgbHistogramFromPixelsOld(groupRgbHist, pixels, cellSizePixels)
+    one32 = cast(1, 'uint32');
+    
     for pixInd=1:length(pixels)
-        pix = pixels(pixInd,:) + 1; % +1 to Matlab index
-        rgb32 = RunEarthMoverDistance1.rgb2uint32(pix(1),pix(2),pix(3));
-        if groupRgbHist.isKey(rgb32)
-            groupRgbHist(rgb32) = groupRgbHist(rgb32) + cast(1, 'uint32');
+        pix = pixels(pixInd,:); % zero based RGB coordinates
+        
+        % quntize - put pixels in cells of specified size
+        % the information is lost but processing perf increases
+        cell = fix(pix ./ cellSizePixels) + 1;
+        
+        cell32 = RunEarthMoverDistance1.rgb2uint32(cell(1),cell(2),cell(3));
+        if groupRgbHist.isKey(cell32)
+            groupRgbHist(cell32) = groupRgbHist(cell32) + one32;
         else
-            groupRgbHist(rgb32) = cast(1, 'uint32');
+            groupRgbHist(cell32) = one32;
         end
     end
 end
 
 % testHistMats = cell of histograms (each in a form of matrix Nx4)
-function testAppearModelOnCandidates(appearModel, testHistMats)
+function testAppearModelOnOtherShapes(appearModel, testHistMats)
     groupsCount = length(appearModel);
     testsCount = length(testHistMats);
     
@@ -248,18 +227,45 @@ function testAppearModelOnCandidates(appearModel, testHistMats)
     display(distMat);
 end
 
-% rgb2uint32(170,187,204)
-function rgbInt = rgb2uint32(r, g, b)
-    r32 = cast(r, 'uint32');
-    g32 = cast(g, 'uint32');
-    b32 = cast(b, 'uint32');
-    rgbInt = bitor(bitor(bitshift(r32, 24), bitshift(g32, 16)), bitshift(b32, 8));
+function analyzeHistogramDistributions(obj, debug)
+    appearModelMap = obj.v.appearModel.values;
+    hist1 = appearModelMap{1};
+    hist(hist1(:,1),1:10)
+    prctile(hist1(:,1), 90)
+    sum(hist1(:,1)<=3)/sum(hist1(:,1))
+end
+
+function rgbInt = rgb2uint32(rgbByRow)
+    rgbByRow32 = cast(rgbByRow, 'uint32');
+
+    rgbInt = bitor(bitor(bitshift(rgbByRow32(:,1), 24), bitshift(rgbByRow32(:,2), 16)), bitshift(rgbByRow32(:,3), 8));
     
     assert(isa(rgbInt,'uint32'));
 end
 
 % [r g b]=uint322rgb(cast(2864434176,'uint32'))
 function [r,g,b] = uint322rgb(rgb32)
+    assert(isa(rgb32,'uint32'));
+    
+    r = bitsra(rgb32, 24);
+%     g = bitshift(rgb32,8);
+%     g = bitsra(g,24);
+%     b = bitshift(rgb32,16);
+%     b = bitsra(b,24);
+    
+    % 4278190080 % FF000000
+    % 16711680   % 00FF0000
+    % 65280      % 0000FF00
+    
+    g = bitsra(bitand(rgb32, 16711680), 16);
+    b = bitsra(bitand(rgb32, 65280), 8);
+    
+    %assert(isa(r,'uint8'));
+    %assert(isa(g,'uint8'));
+    %assert(isa(b,'uint8'));
+end
+
+function [r,g,b] = uint322rgbOld(rgb32)
     assert(isa(rgb32,'uint32'));
     
     r = bitsra(rgb32, 24);
